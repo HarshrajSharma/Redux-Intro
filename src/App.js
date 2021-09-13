f5858bfefe45d6f6fefb1f31b9c4ccdc635b3730
Harsh Sharma <harshrajsharma.2003@gmail.com>
2021-09-13 21:24:03 +0530

    Increament Decreament Using Redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./App.css";
 
+import { useSelector, useDispatch} from 'react-redux';
+import { decNumber, incNumber } from "./actions";
+
 function App(){
+
+  const myState=useSelector((state)=>state.changeNumber);
+  const dispatch=useDispatch  ();
   return(
     <div className="container" >
 
       <h1>Increament/Decreament</h1>
       <h4>using React and Redux</h4>
-      <input></input>
+      <input type="text" value={myState} />
+      <div>
+        <button onClick={()=>{dispatch(decNumber())}} >-</button>
+        <button onClick={()=>{dispatch(incNumber())}} >+</button>
+      </div>
     </div>
   );
 }
